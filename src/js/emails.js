58aef165b4b492d7675d5439fb741f0bd4ca1b2e
Luke Morgan <morgan.luke94@gmail.com>
2021-02-06 00:41:01 +0000

    adds js resources

--- src/js/emails.js
+++ src/js/emails.js
@@ -12,30 +12,12 @@ function enableSubmitButton() {
 
 function submitPortfolioContactForm(event) {
   const form = document.querySelector("#portfolioContactForm");
-  const user_id = "user_YHcDmhol6iTgqix2MNQO3";
-  const service_id = "portfolio_contact_form";
-  const template_id = "landing_site_contact_form_submission";
+
 
   if (form && form.checkValidity()) {
-    emailjs.init(user_id);
-
-    emailjs.sendForm(service_id, template_id, form).then(
-      (response) => {
-        if (response.status == 200) {
-          contactFormBtn.disabled = true;
-          form.reset();
-        }
-      },
-      (error) => {
-        alert(
-          "there was a problem sending your message through the contact form, try again"
-        );
-        contactFormBtn.disabled = false;
-      }
-    );
-  } else {
-    alert("Please ensure all form fields are valid before submitting...");
-  }
+  
+    await fetch('https://')
+  
 }
 
 function convertFormDataToJson(form) {
