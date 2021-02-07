db1b536c728d4a2c02ad2987a5dcd03bc2f5065f
Luke Morgan <morgan.luke94@gmail.com>
2021-02-07 12:44:46 +0000

    tidies up client side js

--- src/js/emails.js
+++ src/js/emails.js
@@ -29,8 +29,8 @@ function submitPortfolioContactForm() {
 
     formRequest
       .then((res) => res.json())
-      .then((data => {
-        console.log(data)
+      .then((data) => {
+        console.log(data);
       })
       .catch((err) => {
         console.log(err);
