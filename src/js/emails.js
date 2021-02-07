7f2c3a5294aa5b4e2524ea75fd30463d4ee313a8
Luke Morgan <morgan.luke94@gmail.com>
2021-02-07 01:03:14 +0000

    fixes emails js

--- src/js/emails.js
+++ src/js/emails.js
@@ -1,6 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
   const contactFormBtn = document.querySelector("#portfolioContactSubmit");
-
   if (contactFormBtn) {
     contactFormBtn.addEventListener("click", submitPortfolioContactForm);
   }
@@ -10,31 +9,32 @@ function enableSubmitButton() {
   return (contactFormBtn.disabled = false);
 }
 
-function submitPortfolioContactForm(event) {
+function submitPortfolioContactForm() {
   const form = document.querySelector("#portfolioContactForm");
-  const user_id = "user_YHcDmhol6iTgqix2MNQO3";
-  const service_id = "portfolio_contact_form";
-  const template_id = "landing_site_contact_form_submission";
 
   if (form && form.checkValidity()) {
-    emailjs.init(user_id);
+    // fetch post
 
-    emailjs.sendForm(service_id, template_id, form).then(
-      (response) => {
-        if (response.status == 200) {
-          contactFormBtn.disabled = true;
-          form.reset();
-        }
-      },
-      (error) => {
-        alert(
-          "there was a problem sending your message through the contact form, try again"
-        );
-        contactFormBtn.disabled = false;
+    const formData = convertFormDataToJson(form);
+    const formRequest = fetch(
+      "https://lukemorgan-web-cv.netlify.app/.netlify/functions/sendMail",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: formData,
       }
     );
-  } else {
-    alert("Please ensure all form fields are valid before submitting...");
+
+    formRequest
+      .then((res) => {
+        res.json();
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
