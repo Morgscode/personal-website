d3a13c8628382dcfcf719f63badcb3756e2019c4
Luke Morgan <morgan.luke94@gmail.com>
2021-02-07 16:07:37 +0000

    swtiches back to res json

--- src/js/emails.js
+++ src/js/emails.js
@@ -26,7 +26,7 @@ function submitPortfolioContactForm() {
         body: formDataJSON,
       }
     )
-      .then((res) => res.text())
+      .then((res) => res.json())
       .then((data) => console.log(data))
       .catch((err) => console.log(err));
   }
