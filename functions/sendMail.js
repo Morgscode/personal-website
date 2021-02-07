f90ed1dfda7baf6f89ffe17d2a4fe76cd4f4fb00
Luke Morgan <morgan.luke94@gmail.com>
2021-02-07 12:15:53 +0000

    swtiches smtp key

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
     secure: true,
     auth: {
       user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      pass: process.env.SMTP_KEY,
     },
     logger: true,
   });
