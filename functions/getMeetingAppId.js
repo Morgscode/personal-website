50a87dcd96413049cc6c3f7cd9ee45ddffad154c
Luke Morgan <luke@tjcuk.co.uk>
2022-08-27 22:16:18 +0100

    add login for protecting meeting app id

---
+++ functions/getMeetingAppId.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+
+exports.handler = async (event, context) => {
+  try {
+    if (!process.env.MEETING_APP_ID) {
+      throw new Error();
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: 'success',
+        statusCode: 200,
+        data: {
+          message: process.env.MEETING_APP_ID,
+        },
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        status: 'success',
+        statusCode: 500,
+        data: {
+          message: 'There is no app id set right now',
+        },
+      }),
+    };
+  }
+};
