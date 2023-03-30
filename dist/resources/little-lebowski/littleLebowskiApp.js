761aeac31a9f11c53d28b26f9b92769b4aceba1f
Luke Morgan <morgan.luke94@gmail.com>
2023-03-31 00:30:16 +0100

    game update

--- dist/resources/little-lebowski/littleLebowskiApp.js
+++ dist/resources/little-lebowski/littleLebowskiApp.js
@@ -4,36 +4,36 @@
 const dataController = (() => {
   const playerJumpState = {
     setJumpCount() {
-      return window.localStorage.setItem('jumpCount', 1);
+      return window.localStorage.setItem("jumpCount", 1);
     },
     resetJumpCount() {
-      return window.localStorage.setItem('jumpCount', 0);
+      return window.localStorage.setItem("jumpCount", 0);
     },
     getJumpCount() {
-      return window.localStorage.getItem('jumpCount');
+      return window.localStorage.getItem("jumpCount");
     },
   };
 
   const scoreState = {
     setGameScore() {
-      return window.localStorage.setItem('playerScore', 0);
+      return window.localStorage.setItem("playerScore", 0);
     },
     getGameScore() {
-      return parseInt(window.localStorage.getItem('playerScore'));
+      return parseInt(window.localStorage.getItem("playerScore"));
     },
   };
 
   const levelState = {
     setupGameLevel() {
-      return window.localStorage.setItem('playerLevel', 1);
+      return window.localStorage.setItem("playerLevel", 1);
     },
     incrementGameLevel() {
-      let level = parseInt(window.localStorage.getItem('playerLevel'), 10);
+      let level = parseInt(window.localStorage.getItem("playerLevel"), 10);
       level++;
-      return window.localStorage.setItem('playerLevel', level);
+      return window.localStorage.setItem("playerLevel", level);
     },
     getGameLevel() {
-      return window.localStorage.getItem('playerLevel');
+      return window.localStorage.getItem("playerLevel");
     },
   };
 
@@ -56,24 +56,24 @@ const uiController = (() => {
   const initialSceneSetupFunctions = {
     loadGameAssets(gameObject) {
       gameObject.load.image(
-        'sky',
-        './dist/resources/little-lebowski/assets/sky.png'
+        "sky",
+        "./dist/resources/little-lebowski/assets/sky.png"
       );
       gameObject.load.image(
-        'ground',
-        './dist/resources/little-lebowski/assets/platform.png'
+        "ground",
+        "./dist/resources/little-lebowski/assets/platform.png"
       );
       gameObject.load.image(
-        'star',
-        './dist/resources/little-lebowski/assets/star.png'
+        "star",
+        "./dist/resources/little-lebowski/assets/star.png"
       );
       gameObject.load.image(
-        'bomb',
-        './dist/resources/little-lebowski/assets/bomb.png'
+        "bomb",
+        "./dist/resources/little-lebowski/assets/bomb.png"
       );
       gameObject.load.spritesheet(
-        'dude',
-        './dist/resources/little-lebowski/assets/dude.png',
+        "dude",
+        "./dist/resources/little-lebowski/assets/dude.png",
         {
           frameWidth: 32,
           frameHeight: 48,
@@ -82,31 +82,31 @@ const uiController = (() => {
       return gameObject;
     },
     renderBlueSkyBackground(gameObject) {
-      gameObject.add.image(0, 0, 'sky').setOrigin(0, 0);
+      gameObject.add.image(0, 0, "sky").setOrigin(0, 0);
       return gameObject;
     },
     renderScenePlatforms(gameObject) {
       const platforms = gameObject.physics.add.staticGroup();
       // ground layer
-      platforms.create(400, 600, 'ground').setScale(2).refreshBody();
+      platforms.create(400, 600, "ground").setScale(2).refreshBody();
       // 2nd left
-      platforms.create(250, 500, 'ground');
+      platforms.create(250, 500, "ground");
       // 2nd right
-      platforms.create(750, 500, 'ground');
+      platforms.create(750, 500, "ground");
       // 3rd left
-      platforms.create(-100, 400, 'ground');
+      platforms.create(-100, 400, "ground");
       // 3rd middle
-      platforms.create(400, 400, 'ground');
+      platforms.create(400, 400, "ground");
       // 3rd right
-      platforms.create(900, 400, 'ground');
+      platforms.create(900, 400, "ground");
       // 4th left
-      platforms.create(145, 275, 'ground');
+      platforms.create(145, 275, "ground");
       // 4th right
-      platforms.create(650, 275, 'ground');
+      platforms.create(650, 275, "ground");
       // 5th left
-      platforms.create(0, 125, 'ground');
+      platforms.create(0, 125, "ground");
       // 5th right
-      platforms.create(500, 125, 'ground');
+      platforms.create(500, 125, "ground");
       return platforms;
     },
     renderSceneBombs(gameObject) {
@@ -115,8 +115,8 @@ const uiController = (() => {
     },
     displayGameTitle(gameObject) {
       const titleCss = {
-        font: '16px Courier',
-        fill: '#fff',
+        font: "16px Courier",
+        fill: "#fff",
       };
       gameObject.add.text(
         300,
@@ -133,7 +133,7 @@ const uiController = (() => {
     renderScoreBoardText(gameObject, score) {
       const scoreText = gameObject.add.text(16, 16, `Score: ${score}`, {
         fontSize: `32px`,
-        fontFamily: 'Courier',
+        fontFamily: "Courier",
         fill: `#303030`,
       });
       return scoreText;
@@ -141,7 +141,7 @@ const uiController = (() => {
     renderGameLevelText(gameObject, level) {
       const levelText = gameObject.add.text(600, 16, `Level: ${level}`, {
         fontSize: `32px`,
-        fontFamily: 'Courier',
+        fontFamily: "Courier",
         fill: `#303030`,
       });
       return levelText;
@@ -151,7 +151,7 @@ const uiController = (() => {
   const starSetupFunctions = {
     renderStarGroup(gameObject, xVal, yVal, stepXVal) {
       const stars = gameObject.physics.add.group({
-        key: 'star',
+        key: "star",
         repeat: 10,
         setXY: { x: xVal, y: yVal, stepX: stepXVal },
       });
@@ -166,9 +166,9 @@ const uiController = (() => {
     },
     starCollected(player, star) {
       star.disableBody(true, true);
-      let score = parseInt(window.localStorage.getItem('playerScore'));
+      let score = parseInt(window.localStorage.getItem("playerScore"));
       score += 10;
-      return window.localStorage.setItem('playerScore', score);
+      return window.localStorage.setItem("playerScore", score);
     },
     renderRandomStarGroup(stars) {
       stars.children.iterate((child) => {
@@ -181,11 +181,11 @@ const uiController = (() => {
 
   const bombSetupFunctions = {
     renderBomb(gameObject) {
-      const bomb = gameObject.physics.add.sprite(400, 300, 'bomb');
+      const bomb = gameObject.physics.add.sprite(400, 300, "bomb");
       return bomb;
     },
     spawnBomb(bombs, xCord) {
-      const bomb = bombs.create(xCord, 16, 'bomb');
+      const bomb = bombs.create(xCord, 16, "bomb");
       bomb.setBounce(1);
       bomb.setCollideWorldBounds(true);
       bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
@@ -199,7 +199,7 @@ const uiController = (() => {
 
   const playerSetupFunctions = {
     renderplayer(gameObject) {
-      const player = gameObject.physics.add.sprite(50, 545, 'dude');
+      const player = gameObject.physics.add.sprite(50, 545, "dude");
       return player;
     },
     setplayerInitialPhysics(player) {
@@ -210,8 +210,8 @@ const uiController = (() => {
     },
     setplayerAnimations(gameObject) {
       gameObject.anims.create({
-        key: 'left',
-        frames: gameObject.anims.generateFrameNumbers('dude', {
+        key: "left",
+        frames: gameObject.anims.generateFrameNumbers("dude", {
           start: 0,
           end: 3,
         }),
@@ -220,14 +220,14 @@ const uiController = (() => {
       });
 
       gameObject.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
+        key: "turn",
+        frames: [{ key: "dude", frame: 4 }],
         frameRate: 10,
       });
 
       gameObject.anims.create({
-        key: 'right',
-        frames: gameObject.anims.generateFrameNumbers('dude', {
+        key: "right",
+        frames: gameObject.anims.generateFrameNumbers("dude", {
           start: 5,
           end: 8,
         }),
@@ -236,7 +236,7 @@ const uiController = (() => {
       });
       return gameObject;
     },
-    setupplayerMovement(player, cursors, dataCtrl) {
+    setupPlayerMovement(player, cursors, dataCtrl) {
       /**
        *
        * Let's asses which cursor is being pressed,
@@ -247,13 +247,13 @@ const uiController = (() => {
        */
       if (cursors.left.isDown) {
         player.setVelocityX(-120);
-        player.anims.play('left', true);
+        player.anims.play("left", true);
       } else if (cursors.right.isDown) {
         player.setVelocityX(120);
-        player.anims.play('right', true);
+        player.anims.play("right", true);
       } else {
         player.setVelocityX(0);
-        player.anims.play('turn');
+        player.anims.play("turn");
       }
 
       /**
@@ -298,7 +298,13 @@ const uiController = (() => {
       return gameObject;
     },
     setupPlayerBombCollision(gameObject, player, bombs, hitBomb, gameOver) {
-      gameObject.physics.add.collider(player, bombs, hitBomb, gameOver, gameObject);
+      gameObject.physics.add.collider(
+        player,
+        bombs,
+        hitBomb,
+        gameOver,
+        gameObject
+      );
       return gameObject;
     },
     setupPlayerStarCollection(player, stars, collectStarFn, gameObject) {
@@ -313,14 +319,14 @@ const uiController = (() => {
     },
     bombHitsPlayer(player, bomb) {
       player.setTint(0xff0000);
-      player.anims.play('turn');
+      player.anims.play("turn");
       player.angle = 180;
       this.physics.pause();
     },
   };
 
   return {
-    loadAssets: initialSceneSetupFunctions.loadGameAssets,
+    loadGameSceneAssets: initialSceneSetupFunctions.loadGameAssets,
     renderSky: initialSceneSetupFunctions.renderBlueSkyBackground,
     renderPlatforms: initialSceneSetupFunctions.renderScenePlatforms,
     renderBombs: initialSceneSetupFunctions.renderSceneBombs,
@@ -338,7 +344,7 @@ const uiController = (() => {
     bombHitsPlayer: playerSetupFunctions.bombHitsPlayer,
     setPlayerStarCollisions: playerSetupFunctions.setupPlayerStarCollection,
     setPlatformBombCollisions: bombSetupFunctions.setupBombPlatformCollision,
-    evaluateplayerMovement: playerSetupFunctions.setupplayerMovement,
+    evaluateplayerMovement: playerSetupFunctions.setupPlayerMovement,
     renderScoreText: initialSceneSetupFunctions.renderScoreBoardText,
     renderLevelText: initialSceneSetupFunctions.renderGameLevelText,
     renderRandomStars: starSetupFunctions.renderRandomStarGroup,
@@ -358,36 +364,179 @@ const gameController = ((uiCtrl, dataCtrl) => {
   let bombs;
   let platforms;
   let stars = [];
+  let overlay;
+  let startText;
+  let titleText;
+  let levelText;
   let scoreText;
   let activeStarGroups;
+  let gameOverText;
+
+  //----- GAME SCENE FUNCTIONS DEFINITIONS
+
+  class StartScene extends Phaser.Scene {
+    constructor() {
+      super({ key: "StartScene" });
+    }
+
+    preload() {
+      uiCtrl.loadGameSceneAssets(this);
+    }
+
+    create() {
+      uiCtrl.renderSky(this);
+      platforms = uiCtrl.renderPlatforms(this);
+
+      overlay = this.add
+        .rectangle(
+          0,
+          0,
+          this.cameras.main.width,
+          this.cameras.main.height,
+          0x000000
+        )
+        .setOrigin(0, 0);
+      overlay.alpha = 0.7; // Adjust the alpha value to control the transparency (0 = fully transparent, 1 = fully opaque)
+
+      titleText = this.add
+        .text(400, 200, "The Adventure(s) of Little Lebowski", {
+          font: "48px Courier",
+          fill: "#fff",
+        })
+        .setOrigin(0.5);
+
+      startText = this.add
+        .text(400, 300, "Start Game", { font: "32px Courier", fill: "#fff" })
+        .setOrigin(0.5)
+        .setInteractive();
+
+      startText.on("pointerdown", () => {
+        this.scene.stop("StartScene");
+        this.scene.start("GameScene");
+      });
+    }
+
+    update() {}
+  }
+
+  class GameScene extends Phaser.Scene {
+    constructor() {
+      super({ key: "GameScene" });
+    }
+
+    preload() {
+      this.scene.destroy("StartScene");
+      uiCtrl.loadGameSceneAssets(this);
+    }
+
+    create() {
+      dataCtrl.setScore();
+      dataCtrl.setupLevel();
+      uiCtrl.renderSky(this);
+      platforms = uiCtrl.renderPlatforms(this);
+      stars.push(uiCtrl.renderStars(this, 5, 450, 70));
+      stars.push(uiCtrl.renderStars(this, 15, 350, 60));
+      stars.push(uiCtrl.renderStars(this, 15, 200, 60));
+      stars.push(uiCtrl.renderStars(this, 25, 50, 50));
+      uiCtrl.setStarPlatformCollisions(this, stars, platforms);
+      player = uiCtrl.renderplayer(this);
+      uiCtrl.setplayerPhysics(player);
+      uiCtrl.setPlayerPlatformCollisions(this, player, platforms);
+      uiCtrl.setplayerAnimations(this);
+      bombs = uiCtrl.renderBombs(this);
+      uiCtrl.setPlatformBombCollisions(this, bombs, platforms);
+      uiCtrl.setPlayerBombCollisions(
+        this,
+        player,
+        bombs,
+        uiCtrl.bombHitsPlayer,
+        triggerGameOver
+      );
+      cursors = uiCtrl.setupCursorKeys(this);
+      uiCtrl.displayTitle(this);
+      scoreText = uiCtrl.renderScoreText(this, dataCtrl.getScore());
+      levelText = uiCtrl.renderLevelText(this, dataCtrl.getLevel());
+    }
+
+    update() {
+      if (gameOver) return;
+
+      uiCtrl.evaluateplayerMovement(player, cursors, dataCtrl);
+      uiCtrl.setPlayerStarCollisions(player, stars, uiCtrl.collectStar, this);
+      scoreText.setText(`Score: ${dataCtrl.getScore()}`);
+      levelText.setText(`Level: ${dataCtrl.getLevel()}`);
+      activeStarGroups = stars.length || 0;
+      stars.forEach((starGroup) => {
+        if (starGroup?.countActive() === 0) {
+          activeStarGroups--;
+        }
+      });
+      if (activeStarGroups === 0) {
+        dataCtrl.levelUp();
+        stars.forEach((starGroup) => {
+          uiCtrl.renderRandomStars(starGroup);
+        });
+      }
+      const level = parseInt(dataCtrl.getLevel(), 10);
+      if (bombs.children.size < level) {
+        for (let i = bombs.children.size; i < level; i++) {
+          let cordBase = level % 2 == 0 ? i * 16 : 575 - i;
+          uiCtrl.spawnBomb(bombs, cordBase);
+        }
+      }
+    }
+  }
+
+  class GameOverScene extends Phaser.Scene {
+    constructor() {
+      super({ key: "GameOverScene" });
+    }
+
+    preload() {}
+
+    create() {
+      overlay = this.add
+        .rectangle(
+          0,
+          0,
+          this.cameras.main.width,
+          this.cameras.main.height,
+          0x000000
+        )
+        .setOrigin(0, 0);
+      overlay.alpha = 0.7; // Adjust the alpha value to control the transparency (0 = fully transparent, 1 = fully opaque)
+
+      gameOverText = this.add
+        .text(400, 300, "Start Game", { font: "58px Courier", fill: "#fff" })
+        .setOrigin(0.5);
+    }
+
+    update() {}
+  }
 
   const config = {
     type: Phaser.AUTO,
-    parent: 'little-lebowski-game',
-    title: 'The Little Lebowski',
+    parent: "little-lebowski-game",
+    title: "The Little Lebowski",
     width: 800,
     height: 600,
     physics: {
-      default: 'arcade',
+      default: "arcade",
       arcade: {
         gravity: { y: 300 },
         debug: false,
       },
     },
-    scene: {
-      preload,
-      create,
-      update,
-    },
+    scene: [StartScene, GameScene, GameOverScene],
   };
 
   let observerOptions = {
     root: null,
-    rootMargin: '0px',
+    rootMargin: "0px",
     threshold: 0,
   };
   let observer = new IntersectionObserver(triggerGame, observerOptions);
-  let observerTarget = document.querySelector('#little-lebowski-game');
+  let observerTarget = document.querySelector("#little-lebowski-game");
   observer.observe(observerTarget);
 
   function triggerGame(entries, observer) {
@@ -408,65 +557,9 @@ const gameController = ((uiCtrl, dataCtrl) => {
 
   function triggerGameOver(player, bombs) {
     gameOver = true;
-    game.scene.stop();
+    game.scene.start("GameOverScene");
     return gameOver;
   }
 
-  //----- GAME SCENE FUNCTIONS DEFINITIONS
-
-  function preload() {
-    uiCtrl.loadAssets(this);
-  }
-
-  function create() {
-    dataCtrl.setScore();
-    dataCtrl.setupLevel();
-    uiCtrl.renderSky(this);
-    platforms = uiCtrl.renderPlatforms(this);
-    stars.push(uiCtrl.renderStars(this, 5, 450, 70));
-    stars.push(uiCtrl.renderStars(this, 15, 350, 60));
-    stars.push(uiCtrl.renderStars(this, 15, 200, 60));
-    stars.push(uiCtrl.renderStars(this, 25, 50, 50));
-    uiCtrl.setStarPlatformCollisions(this, stars, platforms);
-    player = uiCtrl.renderplayer(this);
-    uiCtrl.setplayerPhysics(player);
-    uiCtrl.setPlayerPlatformCollisions(this, player, platforms);
-    uiCtrl.setplayerAnimations(this);
-    bombs = uiCtrl.renderBombs(this);
-    uiCtrl.setPlatformBombCollisions(this, bombs, platforms);
-    uiCtrl.setPlayerBombCollisions(this, player, bombs, uiCtrl.bombHitsPlayer, triggerGameOver);
-    cursors = uiCtrl.setupCursorKeys(this);
-    uiCtrl.displayTitle(this);
-    scoreText = uiCtrl.renderScoreText(this, dataCtrl.getScore());
-    levelText = uiCtrl.renderLevelText(this, dataCtrl.getLevel());
-  }
-
-  function update() {
-    if (gameOver) return;
-
-    uiCtrl.evaluateplayerMovement(player, cursors, dataCtrl);
-    uiCtrl.setPlayerStarCollisions(player, stars, uiCtrl.collectStar, this);
-    scoreText.setText(`Score: ${dataCtrl.getScore()}`);
-    levelText.setText(`Level: ${dataCtrl.getLevel()}`);
-    activeStarGroups = stars.length || 0;
-    stars.forEach((starGroup) => {
-      if (starGroup?.countActive() === 0) {
-        activeStarGroups--;
-      }
-    });
-    if (activeStarGroups === 0) {
-      dataCtrl.levelUp();
-      stars.forEach((starGroup) => {
-        uiCtrl.renderRandomStars(starGroup);
-      });
-    }
-    const level = parseInt(dataCtrl.getLevel(), 10);
-    if (bombs.children.size < level) {
-      for (let i = bombs.children.size; i < level; i++) {
-        let cordBase = level % 2 == 0 ? i * 16 : 575 - i;
-        uiCtrl.spawnBomb(bombs, cordBase);
-      }
-    }
-  }
   return { game, config };
 })(uiController, dataController);
