da752c14c0c84aedb1d822558e9fc055da4249fd
Luke Morgan <luke@tjcuk.co.uk>
2024-03-02 00:11:42 +0000

    refactor: bomb velocity

--- src/ts/little-lebowski/modules/bombs.ts
+++ src/ts/little-lebowski/modules/bombs.ts
@@ -10,11 +10,15 @@ export function spawnBomb(
   xCord = 15,
   yCord = 16,
 ) {
-  const bomb = bombs.create(xCord, yCord, 'bomb');
+  const bomb = bombs.create(
+    xCord,
+    yCord,
+    'bomb',
+  ) as Phaser.Physics.Arcade.Sprite;
   bomb.setBounce(1);
   bomb.setCollideWorldBounds(true);
-  bomb.setVelocity(PhaserMath.Between(-150, 150), 20);
-  bomb.setMaxVelocity(500, 500);
+  bomb.setVelocity(PhaserMath.Between(-150, 150), 200);
+  bomb.setMaxVelocity(300, 500);
   return bomb;
 }
 
